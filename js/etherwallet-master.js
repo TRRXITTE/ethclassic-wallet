4527112041fbbbcee7800c5d9ac9ab5e4e8921f6
traaitt | TRRXITTE <traaitt@icloud.com>
2025-05-08 00:15:37 +0200

    Update etherwallet-master.js

--- js/etherwallet-master.js
+++ js/etherwallet-master.js
@@ -16472,32 +16472,30 @@ function $LogProvider() {
     }
 
     function consoleLog(type) {
-      var console = $window.console || {},
-          logFn = console[type] || console.log || noop,
-          hasApply = false;
-
-      // Note: reading logFn.apply throws an error in IE11 in IE8 document mode.
-      // The reason behind this is that console.log has type "object" in IE8...
+      var console = $window.console || {};
+      var logFn = console[type] || console.log || noop;
+      var hasApply = false;
+    
+      // Detect if .apply can be safely used (important for older IE modes)
       try {
-        hasApply = !!logFn.apply;
-      } catch (e) {}
-
+        hasApply = typeof logFn.apply === 'function';
+      } catch (e) {
+        hasApply = false;
+      }
+    
       if (hasApply) {
         return function() {
-          var args = [];
-          forEach(arguments, function(arg) {
-            args.push(formatError(arg));
-          });
+          var args = Array.prototype.map.call(arguments, formatError);
           return logFn.apply(console, args);
         };
       }
-
-      // we are IE which either doesn't have window.console => this is noop and we do nothing,
-      // or we are IE where console.log doesn't have apply so we log at least first 2 args
+    
+      // Fallback for IE8/9: support up to two arguments
       return function(arg1, arg2) {
-        logFn(arg1, arg2 == null ? '' : arg2);
+        logFn(formatError(arg1), arg2 != null ? formatError(arg2) : '');
       };
     }
+    
   }];
 }
 
